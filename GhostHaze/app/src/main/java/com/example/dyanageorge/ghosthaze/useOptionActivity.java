e97156eab16fdb3b0ba3b1fc5bc03cd198690bc9
Dyana George <dyana.t.s.george@gmail.com>
2018-11-26 11:39:47 -0500

    user option added

---
+++ GhostHaze/app/src/main/java/com/example/dyanageorge/ghosthaze/useOptionActivity.java
@@ -0,0 +1,13 @@
+package com.example.dyanageorge.ghosthaze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class useOptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_use_option);
+    }
+}
